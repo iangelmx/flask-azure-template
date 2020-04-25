1761c4424b8af26261c22ae5be2c4a0f3e33ced8
iangelmx <aramireza1506@alumno.ipn.mx>
2020-04-25 16:28:38 -0500

    tasting the CI with Github and Azure App Service
    Doing some changes to application to check if it changes on out posted app.

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return jsonify(ok=True, description="Deployed :D", status_code=200)
+    return jsonify(ok=True, description="Probando el CI de Github y Azure :D", status_code=200)
 
 if __name__ == '__main__':
     app.run()
